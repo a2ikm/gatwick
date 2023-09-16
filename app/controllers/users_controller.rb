b9d6386b1407378a00b5fb13ca977ae0fadf4021
Masato Ikeda <masato.ikeda@gmail.com>
2023-09-17 00:37:18 +0900

    Move deleted user to deleted_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,16 @@ class UsersController < ApplicationController
   def destroy
     user = User.find_by(id: params[:id])
     if user
-      user.destroy
+      ActiveRecord::Base.transaction do
+        user.destroy!
+        DeletedUser.create!(
+          id: user.id,
+          name: user.name,
+          created_at: user.created_at,
+          updated_at: user.updated_at,
+          deleted_at: Time.zone.now,
+        )
+      end
     end
 
     redirect_to users_url
